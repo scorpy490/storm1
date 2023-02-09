8b0657a979c27f3572bbcc4e3c00b186ce3e9f3a
scorpy490 <scorpy490@gmail.com>
2023-02-09 16:44:12 +0700

    tk-steps

--- tk.js
+++ tk.js
@@ -1,7 +1,8 @@
 addEventListener("keydown", kdown);
 let k=0, s, t
+let step=1
 s = ''
-let txt = ''
+let txt
 //txt='Привет!\nCофт не должен быть платным'
 //document.getElementById('txt1').innerText = txt
 let tm1 //Время начала
@@ -9,10 +10,19 @@ let tm2 //Время завершения
 let err = 0
 let sf = ''
 let p = document.createElement('h2')
+p.style.border = "3px solid blue"
+p.style.paddingLeft = "5px"
+let exesize = new Map
+exesize.set (1, "оа вал лов фол фал лафа вы фад фод дылда лод важл жфла авдл ожда вфды")
+exesize.set (2, "Никакой софт не должен быть платным")
+exesize.set (3, "Бесплатный сыр бывает только в мышеловке")
 
 function start() {
-    s = ''
-    txt = 'Никакой софт не должен быть платным'
+    document.getElementById('resume').innerText = ''
+    document.getElementById('title_resume').innerText = ''
+
+    txt = exesize.get(step)    
+    s = ''    
     err = 0
     sf = ''
     p.id = "txt1"
@@ -21,7 +31,7 @@ function start() {
         sf += '<span id="' + i + '">' + txt[i] + '</span>'
        // if (txt[i] == '\n') s += '</br>'
 
-    }
+    }    
     p.innerHTML = sf
     document.body.prepend(p)
     document.getElementById('bt').style.display="none"
@@ -45,8 +55,20 @@ function  tk1() {
     if (s.length === txt.length) {
         tm2 = Date.now()
         let delta = Math.floor(tm2-tm1)/1000
+        let title_resume = document.getElementById("title_resume")
+        if (err <2) {
+            step++
+            title_resume.style.color = "green"
+            title_resume.innerText = "Задание выполнено"
+        }
+        else {
+            title_resume.style.color = "red"
+            title_resume.innerText = "Задание не выполнено"
+        }
         let res = ('Задание выпонено! '+ '\nЗнаков: '+ pos + '\nВремя: '+ delta+  " сек.\nОшибок: "+ err+ '\nСкорость: '+ Math.floor(pos/delta*60) + " знаков в минуту")
         document.getElementById('resume').innerText = res
+        document.getElementById('bt').style.display=""
+        document.getElementById('bt').disabled=false
     }
 
 }
