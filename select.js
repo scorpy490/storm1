e0738ae29141b3158f68248727edc29e76fec043
Вечкутов Юрий <scorpy490@gmail.com>
2023-03-08 01:06:23 +0700

    resp1

--- select.js
+++ select.js
@@ -1,17 +1,15 @@
 let q = new XMLHttpRequest()
+
 q.open("get", "http://nt32.ru/select.php")
 q.send()
-
 q.onload = resp()
-
-
-
 function resp() {
-    if (q.status != 200) {
-        console.log('Ошибка')
-        return
-    }
-    else {
-        alert(`Готово, получили ${q.response.length} байт`);
-    }
+    //alert(`Готово, получили ${q.response.length} байт`);
+
+    let resp = q.responseText
+    console.log(q.statusText)
+    console.log('Здесь', q.readyState)
+    console.log(resp)
+
 }
+
